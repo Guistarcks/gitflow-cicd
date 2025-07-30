e9f29a32350728ac61de7a64c36e812bb641cb69
Guistarcks <guistarcksoft@gmail.com>
2025-07-30 22:03:31 +0200

    add update

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__tests__/**/*.test.js',
+    '**/?(*.)+(spec|test).js'
+  ],
+  collectCoverageFrom: [
+    'controller/**/*.js',
+    'model/**/*.js',
+    'routes/**/*.js',
+    '*.js',
+    '!node_modules/**',
+    '!coverage/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  setupFilesAfterEnv: [],
+  testTimeout: 10000
+}; 
